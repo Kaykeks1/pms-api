14b3da24d06e524e0ce4dc388c30c41fd42c4409
kayode <kayuskeks@gmail.com>
2022-12-02 01:29:37 +0100

    created auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+// import { JwtService } from "@nestjs/jwt";
+import { config } from "process";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable({})
+export class AuthService {
+    // constructor (prisma: PrismaService) {
+    //     this.prisma = prisma
+    // }
+    // constructor (private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
+    constructor (private prisma: PrismaService) {}
+    async signup(dto: AuthDto) {
+        try {
+            const hash = await argon.hash(dto.password);
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            delete user.hash
+            return user
+            // return this.signToken(user.id, user.email  )
+        } catch(error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
+            }
+            throw error
+        }
+    }
+    async signin(dto: AuthDto) {
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        });
+
+        if (!user) throw new ForbiddenException('Credentials incorrect')
+
+        const passwordMatches = await argon.verify(user.hash, dto.password)
+
+        if (!passwordMatches) throw new ForbiddenException('Credentials incorrect')
+
+        delete user.hash
+        return user
+        // return this.signToken(user.id, user.email  )
+    }
+    // async signToken (
+    //     userId: number,
+    //     email: string
+    // ): Promise<{ access_token: string }> {
+    //     const payload = {
+    //         sub: userId,
+    //         email
+    //     }
+
+    //     const token = await this.jwt.signAsync(payload, {
+    //         expiresIn: '15m',
+    //         secret: this.config.get('JWT_SECRET')
+    //     })
+
+    //     return {
+    //         access_token: token
+    //     }
+    // }
+} 
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+    constructor (config: ConfigService) {
+        super({
+            datasources: {
+                db: {
+                    url: config.get("DATABASE_URL"), // 'postgresql://postgres:123@localhost:5434/my-nestjs-app?schema=public'
+                }
+            }
+        })
+    }
+}
