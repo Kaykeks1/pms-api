8cc0548963749127b385f074c01498f984042eff
kayode <kayuskeks@gmail.com>
2022-12-08 22:59:01 +0100

    connect organization with project

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, HttpCode, HttpStatus, Patch, Post, Param, UseGuards } from "@nestjs/common";
 import { Request } from "express";
 import { ProjectService } from "./project.service";
-import { CreateProjectDto, UpdateProjectDto, ProjectIdParams } from "./dto";
+import { CreateProjectDto, UpdateProjectDto, ProjectIdParams, OrganizationIdParams } from "./dto";
 import { JwtGuard } from '../auth/guard';
 
 @UseGuards(JwtGuard)
-@Controller('project')
+@Controller('/organization/:organization_id/project')
 export class ProjectController {
     constructor (private projectService: ProjectService) {}
 
     @Post('create')
-    async create (@Body() dto: CreateProjectDto) {
+    async create (@Param() params: OrganizationIdParams, @Body() dto: CreateProjectDto) {
+        const { organization_id } = params
         console.log('herherhehre')
-        return await this.projectService.create(dto)
+        return await this.projectService.create(organization_id, dto)
     }
 
     @Patch('/edit/:project_id')
